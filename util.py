96fed705755c7eb463d94c4ba1b670acee1ccb6e
redside100 <andrewpeng00@gmail.com>
2022-04-19 12:31:29 -0400

    utc-4

--- util.py
+++ util.py
@@ -66,8 +66,10 @@ def get_schedule_info():
         consolidated_event_list = []
         for event_list in info:
             for event in event_list:
-                event['start'] = int(datetime.strptime(event['start'], "%Y-%m-%d %H:%M:%S").timestamp())
-                event['end'] = int(datetime.strptime(event['end'], "%Y-%m-%d %H:%M:%S").timestamp())
+                event['start'] = int(datetime.strptime(event['start'], "%Y-%m-%d %H:%M:%S")
+                                     .replace(tzinfo=pytz.FixedOffset(-240)).timestamp())
+                event['end'] = int(datetime.strptime(event['end'], "%Y-%m-%d %H:%M:%S")
+                                   .replace(tzinfo=pytz.FixedOffset(-240)).timestamp())
                 consolidated_event_list.append(event)
 
         cache.set(cache_key, consolidated_event_list, expire=3600)  # 1 hr cache
